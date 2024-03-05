c06720ac75e83c0750281c39a5dafd39d07168f0
JessCP25 <jpayanoc25@gmail.com>
2024-03-05 18:02:41 -0500

    Creando simulación de backend con un archivo json

--- src/app/prueba-1/components/academic/academic.component.ts
+++ src/app/prueba-1/components/academic/academic.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-academic',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, InputNumberModule, ButtonModule, InputTextModule],
   templateUrl: './academic.component.html',
   styleUrl: './academic.component.css',
 })
-export class AcademicComponent { }
+export class AcademicComponent {}
