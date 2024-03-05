69f21d831daafcb54dcf8649172b8629e3b24a06
JessCP25 <jpayanoc25@gmail.com>
2024-03-05 15:53:10 -0500

    Configuración de primeNg

--- src/app/prueba-1/components/personal/personal.component.ts
+++ src/app/prueba-1/components/personal/personal.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Button, ButtonModule } from 'primeng/button';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-personal',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, InputNumberModule, ButtonModule],
   templateUrl: './personal.component.html',
   styleUrl: './personal.component.css',
 })
-export class PersonalComponent { }
+export class PersonalComponent {}
