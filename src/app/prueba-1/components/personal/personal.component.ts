cecc534a008aa937b7de11a1bc2932fc85b011fe
JessCP25 <jpayanoc25@gmail.com>
2024-03-05 20:32:14 -0500

    Ingresando información para simular consultas

--- src/app/prueba-1/components/personal/personal.component.ts
+++ src/app/prueba-1/components/personal/personal.component.ts
@@ -1,13 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-personal',
   standalone: true,
-  imports: [CommonModule, InputNumberModule, ButtonModule, InputTextModule],
+  imports: [
+    CommonModule,
+    InputNumberModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './personal.component.html',
   styleUrl: './personal.component.css',
 })
-export class PersonalComponent {}
+export class PersonalComponent {
+  constructor(private fb: FormBuilder){
+    this.personalInformationForm.disable();
+  }
+
+  personalInformationForm = this.fb.group({
+    name: ['',[Validators.required]],
+    lastName: ['', [Validators.required]],
+    age: ['', [Validators.required]],
+    city: ['',[Validators.required]]
+  })
+}
